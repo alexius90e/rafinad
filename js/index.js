bd4aa9943ae768c83a5465793635060e35bf2c1b
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-08-01 15:23:40 +0300

    feat: implemented maps

--- js/index.js
+++ js/index.js
@@ -113,3 +113,33 @@ export const specificationSwiper = new Swiper('#specificationSlider', {
     swiper: specificationThumbs,
   },
 });
+
+const carButton = document.querySelector('.maps__descripion_car .button');
+
+const transportButton = document.querySelector('.maps__descripion_transport .button');
+
+const buttons = [carButton, transportButton];
+
+const carMap = document.querySelector('.maps__map_car');
+
+const transportMap = document.querySelector('.maps__map_transport');
+
+const maps = [carMap, transportMap];
+
+carButton.addEventListener('click', (event) => {
+  buttons.forEach((button) => {
+    button.classList.toggle('button_light');
+    button.classList.toggle('button_dark');
+  });
+
+  maps.forEach((map) => map.classList.toggle('active'));
+});
+
+transportButton.addEventListener('click', (event) => {
+  buttons.forEach((button) => {
+    button.classList.toggle('button_light');
+    button.classList.toggle('button_dark');
+  });
+
+  maps.forEach((map) => map.classList.toggle('active'));
+});
