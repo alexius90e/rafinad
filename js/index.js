b83d254c7d31fb161c9ba4ea961f07b0101eb62e
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-08-02 21:37:47 +0300

    deploy: version 1.2.0

--- js/index.js
+++ js/index.js
@@ -113,3 +113,51 @@ export const specificationSwiper = new Swiper('#specificationSlider', {
     swiper: specificationThumbs,
   },
 });
+
+const carButton = document.querySelector('.maps__descripion_car .button');
+
+const transportButton = document.querySelector('.maps__descripion_transport .button');
+
+const buttons = [carButton, transportButton];
+
+const carMap = document.querySelector('.maps__map_car');
+
+const transportMap = document.querySelector('.maps__map_transport');
+
+const maps = [carMap, transportMap];
+
+carButton.addEventListener('click', (event) => {
+  buttons.forEach((button) => {
+    button.classList.toggle('button_light');
+    button.classList.toggle('button_dark');
+  });
+
+  maps.forEach((map) => map.classList.toggle('active'));
+});
+
+transportButton.addEventListener('click', (event) => {
+  buttons.forEach((button) => {
+    button.classList.toggle('button_light');
+    button.classList.toggle('button_dark');
+  });
+
+  maps.forEach((map) => map.classList.toggle('active'));
+});
+
+// apartments
+
+const filterToggler = document.querySelector('.apartments__filter-button');
+
+const filterLayout = document.querySelector('.apartments__layout');
+
+const apartments = document.querySelector('.apartments');
+
+const filter = document.querySelector('.apartments__filter');
+
+filterToggler.addEventListener('click', () => {
+  apartments.classList.toggle('active');
+});
+
+filterLayout.addEventListener('click', () => {
+  apartments.classList.remove('active');
+});
