7ca93cb78c285414148135f37421fd11c22d06f2
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-08-03 02:57:05 +0300

    deploy: version 1.2.3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
-export const firstScreenThumbs = new Swiper('#firstScreenThumbs', {
-  loop: true,
-  spaceBetween: 8,
-  freeMode: true,
-  watchSlidesProgress: true,
-  centerInsufficientSlides: true,
+export const firstScreen = document.querySelector('.first-screen');
 
+firstScreen.addEventListener('pointermove', (event) => {
+  const firstScreenParalax = event.currentTarget.querySelector('.first-screen__paralax');
+  const bg = firstScreenParalax.querySelector('.first-screen-bg');
+  const primaryLayer = firstScreenParalax.querySelector('.first-screen-primary');
+  const secondaryLayer = firstScreenParalax.querySelector('.first-screen-secondary');
+
+  const layers = [
+    { element: bg, speed: 0.01 },
+    { element: primaryLayer, speed: 0.06 },
+    { element: secondaryLayer, speed: 0.03 },
+  ];
+
+  const parallaxLeftOffset = firstScreenParalax.getBoundingClientRect().left;
+  const parallaxTopOffset = firstScreenParalax.getBoundingClientRect().top;
+
+  const coordX = event.clientX - parallaxLeftOffset - 0.5 * firstScreenParalax.offsetWidth;
+  const coordY = event.clientY - parallaxTopOffset - 0.5 * firstScreenParalax.offsetHeight;
+
+  layers.forEach((layer) => {
+    if (layer.element) {
+      const layerSpeed = layer.speed;
+      const x = -(coordX * layerSpeed).toFixed(2);
+      const y = -(coordY * layerSpeed).toFixed(2);
+      layer.element.setAttribute('style', `transform: translate(${x}px, ${y}px) scale(1.08);`);
+    }
+  });
+});
+
+export const firstScreenSlider = new Swiper('#firstScreenSlider', {
+  loop: true,
+  spaceBetween: 11,
+  freeMode: true,
   breakpoints: {
     320: {
       slidesPerView: 2,
@@ -22,54 +49,16 @@ export const firstScreenThumbs = new Swiper('#firstScreenThumbs', {
       slidesPerView: 6.5,
     },
   },
-});
-
-export const firstScreenSwiper = new Swiper('#firstScreenSlider', {
-  loop: true,
-  allowTouchMove: false,
-  pagination: {
-    el: '#firstScreenSlider .swiper-pagination',
-  },
   navigation: {
-    nextEl: '.first-screen .swiper-button-next',
-    prevEl: '.first-screen .swiper-button-prev',
-  },
-  thumbs: {
-    swiper: firstScreenThumbs,
+    nextEl: '#firstScreenSlider .swiper-button-next',
+    prevEl: '#firstScreenSlider .swiper-button-prev',
   },
 });
 
-export const firstScreen = document.querySelector('.first-screen');
-
-firstScreen.addEventListener('pointermove', (event) => {
-  const firstScreenSlides = event.currentTarget.querySelectorAll('.first-screen__slide');
-
-  const activeSlide = firstScreenSlides[firstScreenSwiper.activeIndex];
-
-  const bg = activeSlide.querySelector('.first-screen-bg');
-  const primaryLayer = activeSlide.querySelector('.first-screen-primary');
-  const secondaryLayer = activeSlide.querySelector('.first-screen-secondary');
-
-  const layers = [
-    { element: bg, speed: 0.01 },
-    { element: primaryLayer, speed: 0.06 },
-    { element: secondaryLayer, speed: 0.03 },
-  ];
-
-  const parallaxLeftOffset = activeSlide.getBoundingClientRect().left;
-  const parallaxTopOffset = activeSlide.getBoundingClientRect().top;
-
-  const coordX = event.clientX - parallaxLeftOffset - 0.5 * activeSlide.offsetWidth;
-  const coordY = event.clientY - parallaxTopOffset - 0.5 * activeSlide.offsetHeight;
-
-  layers.forEach((layer) => {
-    if (layer.element) {
-      const layerSpeed = layer.speed;
-      const x = -(coordX * layerSpeed).toFixed(2);
-      const y = -(coordY * layerSpeed).toFixed(2);
-      layer.element.setAttribute('style', `transform: translate(${x}px, ${y}px) scale(1.08);`);
-    }
-  });
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Thumbs: {
+    type: 'classic',
+  },
 });
 
 export const specificationThumbs = new Swiper('#specificationThumbs', {
